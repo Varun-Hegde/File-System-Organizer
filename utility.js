197b9d38d13bf750eec5efa84dd049c92f2d8907
Varun Hegde <varunhegde2k@gmail.com>
2021-05-29 00:17:50 +0530

    Organize and help functions

---
+++ utility.js
@@ -0,0 +1,10 @@
+//For organizing files
+let utility = {};
+utility.types = {
+	media: ['mp4', 'mkv'],
+	archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', 'xz'],
+	documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex', 'rtf'],
+	codes: ['cpp', 'c', 'py', 'js', 'css', 'html', 'ts', 'jsx', 'tsx', 'kt', 'java'],
+	app: ['exe', 'dmg', 'pkg', 'deb'],
+};
+module.exports = utility;
