6524831db836cc56bd5c5a659a8caa7f69e926c1
Varun Hegde <varunhegde2k@gmail.com>
2021-05-29 11:58:57 +0530

    Added Tree Function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
 const utility = require('./utility');
@@ -25,7 +26,37 @@ switch (command) {
 }
 
 function treeFn(dirPath) {
-	console.log('Tree command implemented for ', dirPath);
+	if (!dirPath) {
+		//console.log('Please enter the path');
+		//If no path is given use current working directory
+		treeHelper(process.cwd(), '');
+		return;
+	}
+	let doesExist = fs.existsSync(dirPath);
+	if (!doesExist) {
+		console.log('Please enter the correct path');
+		return;
+	}
+
+	treeHelper(dirPath, '');
+}
+
+//Uses recursion
+function treeHelper(dirPath, indent) {
+	// is file or folder
+	let isFile = fs.lstatSync(dirPath).isFile();
+	if (isFile) {
+		let fileName = path.basename(dirPath);
+		console.log(indent + '├──' + fileName);
+	} else {
+		let dirName = path.basename(dirPath);
+		console.log(indent + '└──' + dirName);
+		let children = fs.readdirSync(dirPath);
+		for (let i = 0; i < children.length; i++) {
+			let childPath = path.join(dirPath, children[i]);
+			treeHelper(childPath, indent + '\t');
+		}
+	}
 }
 
 function orgFn(dirPath) {
@@ -37,7 +68,8 @@ function orgFn(dirPath) {
 
 	//1.
 	if (!dirPath) {
-		console.log('Please enter the path');
+		//If no path is given use current working directory
+		dirPath = process.cwd();
 		return;
 	}
 	let doesExist = fs.existsSync(dirPath);
